41df62599f559905c91e095260e0b0d0024af0bd
Sarvesh <sarveshjaiofficial@gmail.com>
2024-02-02 11:53:03 +0530

    created react project

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import {BrowserRouter,Route,Routes} from "react-router-dom"
+import Home from "./components/Home"
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+      <Routes>
+        <Route element={<Home/>}  path="/" />
+      </Routes>
+      </BrowserRouter>
+    </>
+    )
+}
+
+export default App
