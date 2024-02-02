2fcdb4e2494288596b638333e70fbd79d7168b1f
Sarvesh <sarveshjaiofficial@gmail.com>
2024-02-02 17:12:41 +0530

    coded landing page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,14 @@
-import {BrowserRouter,Route,Routes} from "react-router-dom"
+import {Route,Routes} from "react-router-dom"
 import Home from "./components/Home"
+import NavBar from "./components/NavBar"
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+    <NavBar/>
       <Routes>
         <Route element={<Home/>}  path="/" />
       </Routes>
-      </BrowserRouter>
     </>
     )
 }
---
+++ Frontend/src/components/NavBar.jsx
@@ -0,0 +1,78 @@
+import  { useState } from "react";
+import { Link } from "react-router-dom";
+
+function NavBar() {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <>
+      <nav className="relative select-none bg-blue-300 lg:flex lg:items-stretch w-full">
+        <div className="flex flex-no-shrink items-stretch h-12">
+          <Link
+            to="/"
+            className="flex-no-grow md:text-3xl text-xl font-bold flex-no-shrink relative py-2 px-4 leading-normal text-black no-underline flex items-center hover:bg-grey-dark"
+          >
+            Myth Buster
+          </Link>
+
+          <button
+            onClick={toggleMenu}
+            className="block lg:hidden cursor-pointer ml-auto relative w-12 h-12 p-4"
+          >
+            {isMenuOpen ? (
+              <svg
+                className="fill-current text-black"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
+              </svg>
+            ) : (
+              <svg
+                className="fill-current text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+              </svg>
+            )}
+          </button>
+        </div>
+
+        <div
+          className={`${
+            isMenuOpen ? "block" : "hidden"
+          } lg:flex lg:items-stretch lg:flex-no-shrink lg:flex-grow`}
+        >
+          <div className="lg:flex lg:items-stretch lg:justify-end ml-auto">
+            <Link
+              to="myths"
+              className="flex-no-grow flex-no-shrink relative py-2 px-4 leading-normal text-blue-700 no-underline  font-semibold flex items-center hover:bg-grey-dark"
+            >
+              Myths
+            </Link>
+            <Link
+              to="signin"
+              className="flex-no-grow flex-no-shrink relative py-2 px-4 leading-normal text-white no-underline text-black font-semibold flex items-center hover:bg-grey-dark"
+            >
+              <button className="bg-white p-2 rounded-3xl text-blue-500 font-bold hover:scale-105">Sign Up</button>
+            </Link>
+            <Link
+              to="login"
+              className="flex-no-grow text-black flex-no-shrink relative py-2 px-4 leading-normal text-white no-underline font-semibold flex items-center hover:bg-grey-dark"
+            >
+                <button className="bg-blue-600 p-2 rounded-3xl text-white hover:scale-105">Log In</button>
+              
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default NavBar;
