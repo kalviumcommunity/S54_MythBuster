2b8e38e1d86d6db9c4f0d659f03bbfa3e20ba29b
Sarvesh <sarveshjaiofficial@gmail.com>
2024-02-05 16:41:55 +0530

    Implemented CRUD

--- Backend/routes/postRoutes.js
+++ Backend/routes/postRoutes.js
@@ -1,10 +1,14 @@
 import express from "express"
-
+import {getMythData,findMythById,addMyth,updateMythById,deleteMythById} from "../controllers/postController.js"
 
 const router = express.Router()
 
 
-router.get("/",)
+router.get("/myths",getMythData)
+router.get("/myths/:id",findMythById)
+router.post("/myths",addMyth)
+router.post("/myths/:id",updateMythById)
+router.delete("/myths/:id",deleteMythById)
 
 
 export default router
